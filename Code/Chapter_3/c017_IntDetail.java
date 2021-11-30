708a49c001da21d3cc2682e604da03cb98eee67a
badangle520 <vp23@qq.com>
2021-11-30 23:56:20 +0800

    update

--- Code/Chapter_3/c017_IntDetail.java
+++ Code/Chapter_3/c017_IntDetail.java
@@ -1,5 +1,10 @@
 public class c017_IntDetail{
-	public static void main(String[] args){
 	
+	//编写一个mian方法
+	public static void main(String[] args){
+		//Java的整型常量(具体值)默认为int型,声明long型常量需后加 l 或 L
+		int n1 = 1;    //4个字节
+		//int n2 = 1L; //错误
+		long n3 = 1L;  //正确
 	}
 }
